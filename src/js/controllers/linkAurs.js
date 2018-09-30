9bdbe6031276a8ac4c921e0ca24916d0beca8cb2
prescun <prescun@babys-MacBook-Air.local>
2018-09-30 13:24:11 +0800

    camera saves data to blob and http request is sent to dummy url. logging is 80% clean

--- src/js/controllers/linkAurs.js
+++ src/js/controllers/linkAurs.js
@@ -1,12 +1,13 @@
 'use strict';
 
-angular.module('copayApp.controllers').controller('linkAursController', function($rootScope, $scope, $interval, $filter, $timeout, $ionicScrollDelegate, gettextCatalog, walletService, platformInfo, lodash, configService, $stateParams, $window, $state, $log, profileService, $ionicModal, popupService, $ionicHistory, $ionicConfig, $ionicPopup, $window) {
+angular.module('copayApp.controllers').controller('linkAursController', function($rootScope, $scope, $http, $httpParamSerializer, $interval, $filter, $timeout, $ionicScrollDelegate, gettextCatalog, walletService, platformInfo, lodash, configService, $stateParams, $window, $state, $log, profileService, $ionicModal, popupService, $ionicHistory, $ionicConfig, $ionicPopup, $window, $log) {
 
   $scope.pincode = 0000;
   $scope.formA = {
     phone: '',
     name: '',
-    email: ''
+    email: '',
+    img: null
   }
   $scope.$on("$ionicView.beforeLeave", function(event, data) {
     
@@ -25,130 +26,36 @@ angular.module('copayApp.controllers').controller('linkAursController', function
   $scope.openCamera = function() {
     console.error("opening camera")
     console.error(JSON.stringify(navigator.camera))
-    navigator.camera.getPicture(function cameraSuccess(imageUri) {
-      console.log(imageUri)
-      console.log(imageUri)      
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-      console.log(imageUri)
-      console.log(imageUri)      
-
-
-
-      $window.resolveLocalFileSystemURL(cordova.file.cacheDirectory, function success(dirEntry) {
-
-          // JPEG file
-          dirEntry.getFile("tempFile.jpeg", { create: true, exclusive: false }, function (fileEntry) {
-
-              // Do something with it, like write to it, upload it, etc.
-              // writeFile(fileEntry, imgUri);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              console.log("got file: " + fileEntry.fullPath);
-              // displayFileData(fileEntry.fullPath, "File copied to");
-
-          }, onErrorCreateFile);
-
-      }, onErrorResolveUrl);
+    var URL = "http://seed.aureus.live/"
+    navigator.camera.getPicture(function cameraSuccess(imageData) {
+      $scope.formA.img = imageData;
+      $http({
+        method: 'POST',
+        url: URL,
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        data: $httpParamSerializer($scope.formA)
+      }).then(function() {
+        $log.info("SUCCESS: KYC sent");
+        return onSuccessConfirm();
+      }, function(err) {
+        $log.info("ERROR: KYC NOT SENT.", err);
+        return onSuccessConfirm();
+      });
 
     }, function cameraError(error) {
         console.log("Unable to obtain picture: " + error, "app");
     }, {
           // Some common settings are 20, 50, and 100
           quality: 100,
-          destinationType: Camera.DestinationType.FILE_URI,
+          destinationType: Camera.DestinationType.DATA_URL,
           // In this app, dynamically set the picture source, Camera or photo gallery
           sourceType: Camera.PictureSourceType.CAMERA,
           encodingType: Camera.EncodingType.JPEG,
           mediaType: Camera.MediaType.PICTURE,
-          allowEdit: true,
-          correctOrientation: true  //Corrects Android orientation quirks
+          allowEdit: false,
+          correctOrientation: false  //Corrects Android orientation quirks
       });
   }
   $scope.onSuccessConfirm = function() {

dc21ea6dc271bc05b9d85b6686cf6f35bfc8b16a
prescun <prescun@babys-MacBook-Air.local>
2018-10-01 01:51:27 +0800

    set up connector to server for PIN number and error handling for APIs

--- src/js/controllers/linkAurs.js
+++ src/js/controllers/linkAurs.js
@@ -2,7 +2,8 @@
 
 angular.module('copayApp.controllers').controller('linkAursController', function($rootScope, $scope, $http, $httpParamSerializer, $interval, $filter, $timeout, $ionicScrollDelegate, ionicToast, gettextCatalog, walletService, platformInfo, lodash, configService, $stateParams, $window, $state, $log, profileService, $ionicModal, popupService, $ionicLoading, $ionicHistory, $ionicConfig, $ionicPopup, $window, $log) {
 
-  $scope.pincode = 0000;
+  $scope.pincode = null;
+  $scope.uuid = null;
   $scope.formA = {
     phone: '',
     name: '',
@@ -14,36 +15,64 @@ angular.module('copayApp.controllers').controller('linkAursController', function
   });
 
   $scope.$on("$ionicView.enter", function(event, data) {
-    
+    window.plugins.uniqueDeviceID.get(
+      function(uuid) {
+        $scope.uuid = uuid;
+        $scope.getNewPin()
+      }, function() {
+        ionicToast.show(gettextCatalog.getString('Unable to contact verification server. Please try again.'), 'middle', false, 2000);    
+        $scope.goBack()
+      }); 
   });
-
 
   $scope.$on("$ionicView.beforeEnter", function(event, data) {
-
-    
+     
   });
+
+  $scope.getNewPin = function() {
+    var URL = 'http://seed.aureus.live/api/verification'
+    $http({
+      method: 'POST',
+      url: URL,
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      data: $httpParamSerializer({appId: $scope.uuid})
+    }).then(function() {
+      $log.info("SUCCESS: Verification PIN retrieved");
+      $ionicLoading.hide();
+      ionicToast.show(gettextCatalog.getString('Verification uploaded. Your info will be processed.'), 'middle', false, 2000);
+      return $scope.goBack();
+    }, function(err) {
+      $ionicLoading.hide();
+      $log.info("ERROR: Verification PIN NOT RETRIEVED.", err);
+      popupService.showAlert(gettextCatalog.getString('Error'), "Network error sending verification info");
+    });        
+  }  
   
   $scope.openCamera = function() {
     $ionicLoading.show({ template: "Uploading verification data..." })
 
-    var URL = "http://seed.aureus.live/"
+
     navigator.camera.getPicture(function cameraSuccess(imageData) {
       $scope.formA.img = imageData;
+      $scope.formA.uuid = $scope.uuid;
+      var URL = "http://seed.aureus.live/api/verification"
       $http({
-        method: 'POST',
+        method: 'PUT',
         url: URL,
         headers: {
           'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
         },
         data: $httpParamSerializer($scope.formA)
       }).then(function() {
-        $log.info("SUCCESS: KYC sent");
+        $log.info("SUCCESS: Verification sent");
         $ionicLoading.hide();
         ionicToast.show(gettextCatalog.getString('Verification uploaded. Your info will be processed.'), 'middle', false, 2000);
-        return $scope.onSuccessConfirm();
+        return $scope.goBack();
       }, function(err) {
         $ionicLoading.hide();
-        $log.info("ERROR: KYC NOT SENT.", err);
+        $log.info("ERROR: Verification NOT SENT.", err);
         popupService.showAlert(gettextCatalog.getString('Error'), "Network error sending verification info");
       });
 
@@ -61,9 +90,9 @@ angular.module('copayApp.controllers').controller('linkAursController', function
           mediaType: Camera.MediaType.PICTURE,
           allowEdit: false,
           correctOrientation: false  //Corrects Android orientation quirks
-      });
+    });
   }
-  $scope.onSuccessConfirm = function() {
+  $scope.goBack = function() {
     $ionicHistory.nextViewOptions({
       disableAnimate: true,
       historyRoot: true
@@ -71,7 +100,4 @@ angular.module('copayApp.controllers').controller('linkAursController', function
     $ionicHistory.clearHistory();
     $state.transitionTo('tabs.home');
   };
-
-  
-
 });
